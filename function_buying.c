7867fbace64bb0e948031fb1dcc7cbafa646d643
Oleksandr <sashatroan@gmail.com>
2023-07-12 20:14:38 +0300

    Added basket to the buying function. Also added list of products.

--- function_buying.c
+++ function_buying.c
@@ -1,34 +1,89 @@
 #include <stdbool.h>
 #include <conio.h>
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 typedef struct {
-    char goods_name[30];
-    int goods_cost;
+    char goods_name[70];
+    float goods_cost;
 } basket;
 
 void adding_goods();
+void basket_view(int product_count);
 
-int function_buying(){
-    int menu_buy_item;
+int function_buying(int login_is_true){
+    int menu_buy_item, product_count = 0;
+    remove("basket.txt");
+    FILE *f = fopen("basket.txt", "wt");
+    fclose(f);
     printf("Ви обрали купівлю товару!\n\n");
-    while (true) {
-        printf("Оберіть, що ви хочете робити:\n 1. Додати товари до кошика\n 2. Переглянути кошик(і суму)\n 3. Купити все, що в кошику\n 4. Очистити кошик\n 5. Повернутися до меню\n\n");
-        menu_buy_item = getch();
-        switch (menu_buy_item) {
-            case '1': adding_goods();
-            case '2':
-            case '3':
-            case '4':
-            case '5': return 0;
-            default:
-                printf("\nВиберіть коректну цифру\n");
+    if (login_is_true == 1) {
+        while (true) {
+            printf("Оберіть, що ви хочете робити:\n 1. Додати товари до кошика\n 2. Переглянути кошик(і суму)\n 3. Купити все, що в кошику\n 4. Очистити кошик\n 5. Повернутися до меню\n\n");
+            menu_buy_item = getch();
+            switch (menu_buy_item) {
+                case '1':
+                    adding_goods();
+                    product_count++;
+                    break;
+                case '2':
+                    basket_view(product_count);
+                    break;
+                case '3':
+                case '4':
+                    remove("basket.txt");
+                    break;
+                case '5':
+                    remove("basket.txt");
+                    return 0;
+                default:
+                    printf("\nВиберіть коректну цифру\n");
+            }
         }
+    } else{
+        printf("\nСпочатку треба увійти до аккаунту або пройти реєстрацію!\n");
+        return 1;
     }
 }
 
 void adding_goods(){
-    basket list[30];
-    FILE *f = fopen("basket.txt", "a");
+    basket list;
+    int goods_code, entered_code;
+    char line[100];
+    char* token;
+    FILE *basketf = fopen("basket.txt", "a");
+    FILE *product_list = fopen("basket_search_list.txt", "rt");
+    printf("Для того, щоб додати їжу, введіть код!\n");
+    scanf("%d", &entered_code);
+    for (int i = 0; i < 52; i++) {
+        fgets(line, sizeof(line), product_list);
+        token = strtok(line, "\t");
+        goods_code = atoi(token);
+        token = strtok(NULL, "\t");
+        strcpy(list.goods_name, token);
+        token = strtok(NULL, "\t");
+        token = strtok(NULL, "\t");
+        list.goods_cost = atof(token);
+        if (entered_code == goods_code){
+            break;
+        }
+    }
+    printf("%d %s %.2f\n\n", goods_code, list.goods_name, list.goods_cost);
+    fwrite(&list.goods_name, sizeof(list.goods_name), 1, basketf);
+    fwrite(&list.goods_cost, sizeof(list.goods_cost), 1, basketf);
+    fclose(basketf);
+    fclose(product_list);
+}
 
+void basket_view(int product_count){
+    basket list;
+    FILE *basket = fopen("basket.txt", "rt");
+    for (int i = 0; i < product_count; i++){
+        fread(list.goods_name, sizeof(list.goods_name), 1, basket);
+        fread(&list.goods_cost, sizeof(float), 1, basket);
+        printf("%s\t", list.goods_name);
+        printf("%.2f\n", list.goods_cost);
+    }
+    fclose(basket);
 }
--- main.c
+++ main.c
@@ -4,10 +4,10 @@
 #include <conio.h>
 #include <string.h>
 
-int function_buying();
+int function_buying(int login_is_true);
 
 int main() {
-    int menu_item;
+    int menu_item, login_is_true = 1;
     printf("Вітаємо вас у нашому застосунку сервісу замовлення їжі!\n");
     while (true) {
         printf("Оберіть пункт меню для того щоб продовжити:\n 1. Дії щодо аккаунта\n 2. Інформація щодо товарів\n 3. Купівля\n 4. Вихід\n");
@@ -16,9 +16,11 @@ int main() {
         switch (menu_item) {
             case '1':
             case '2':
-            case '3': function_buying();
+            case '3':
+                function_buying(login_is_true);
                 break;
-            case '4': exit(1);
+            case '4':
+                exit(1);
             default:
                 printf("\nВиберіть коректну цифру\n");
         }
