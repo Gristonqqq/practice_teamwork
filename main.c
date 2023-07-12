c832eeac81ed1dbd4ce6cf64cd8378b8b1461cd5
Oleksandr <sashatroan@gmail.com>
2023-07-12 13:32:32 +0300

    Create menu and switch condition

--- main.c
+++ main.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <conio.h>
+#include <string.h>
+
+int function_buying();
 
 int main() {
-    printf("Hello, World!\n");
-    return 0;
+    int menu_item;
+    printf("Вітаємо вас у нашому застосунку сервісу замовлення їжі!\n");
+    while (true) {
+        printf("Оберіть пункт меню для того щоб продовжити:\n 1. Дії щодо аккаунта\n 2. Інформація щодо товарів\n 3. Купівля\n 4. Вихід\n");
+        printf("Введіть цифру пункту меню:\n");
+        menu_item = getch();
+        switch (menu_item) {
+            case '1':
+            case '2':
+            case '3': function_buying();
+                break;
+            case '4': exit(1);
+            default:
+                printf("\nВиберіть коректну цифру\n");
+        }
+    }
 }
