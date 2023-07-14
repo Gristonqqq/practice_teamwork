1da5984cb323eea5b725473d8c44c06382acaaa4
Вероніка Левіна <ipz22-v.levina@nubip.edu.ua>
2023-07-14 12:39:54 +0300

    Feature creation and bug fixes

--- function_list_of_products.c
+++ function_list_of_products.c
@@ -1,66 +1,12 @@
 #include <stdio.h>
 #include <string.h>
 
-int function_list_of_products () {
-    int choice;
-    char filename[100];
-
-    printf("Каталог меню:\n");
-    printf("1. Фрукти та овочі\n");
-    printf("2. М'ясо та риба\n");
-    printf("3. Вода\n");
-    printf("4. Сік та безалкогольні напої\n");
-    printf("5. Молочні продукти та яйця\n");
-    printf("6. Хлібні вироби\n");
-    printf("7. Бакалії, крупи, та макаронні вироби\n");
-    printf("0. Вихід\n");
-
-    printf("Виберіть пункт меню: ");
-    scanf("%d", &choice);
-
-    switch (choice) {
-        case 0:
-            printf("Ви обрали вихід. До побачення!\n");
-            break;
-        case 1:
-            printf("Ви обрали 'Фрукти та овочі'.\n");
-            strcpy(filename, "Fruits_vegetables.txt");
-            break;
-        case 2:
-            printf("Ви обрали 'М'ясо та риба'.\n");
-            strcpy(filename, "Meat_fish.txt");
-            break;
-        case 3:
-            printf("Ви обрали 'Вода'.\n");
-            strcpy(filename, "water.txt");
-            break;
-        case 4:
-            printf("Ви обрали 'Сік та безалкогольні напої'.\n");
-            strcpy(filename, "Juice_soft_drinks.txt");
-            break;
-        case 5:
-            printf("Ви обрали 'Молочні продукти та яйця'.\n");
-            strcpy(filename, "Dairy_products_eggs.txt");
-            break;
-        case 6:
-            printf("Ви обрали 'Хлібні вироби'.\n");
-            strcpy(filename, "bread_products.txt");
-            break;
-        case 7:
-            printf("Ви обрали 'Бакалії, крупи, та макаронні вироби'.\n");
-            strcpy(filename, "Groceries_cereals_pasta.txt");
-            break;
-        default:
-            printf("Невірний вибір. Спробуйте ще раз.\n");
-            return 0;
-    }
-
+void readAndProcessFile(const char *filename) {\
     FILE *file = fopen(filename, "r");
     if (file != NULL) {
-        // Читання та обробка файлу
         printf("Файл '%s' відкрито успішно.\n", filename);
 
-        // Перевірка наявності вмісту
+
         fseek(file, 0, SEEK_END);
         long size = ftell(file);
         if (size > 0) {
@@ -76,6 +22,65 @@ int function_list_of_products () {
         fclose(file);
     } else {
         printf("Помилка відкриття файлу '%s'.\n", filename);
+    }
+}
+
+void printMenu() {
+    const char *arr[7] = {
+            "1. Фрукти та овочі\n",
+            "2. М'ясо та риба\n",
+            "3. Вода\n",
+            "4. Сік та безалкогольні напої\n",
+            "5. Молочні продукти та яйця\n",
+            "6. Хлібні вироби\n",
+            "7. Бакалії, крупи, та макаронні вироби\n"
+    };
+
+    printf("Каталог меню:\n");
+
+    for (int i = 0; i < 7; i++) {
+        printf("%s", arr[i]);
+    }
+}
+
+int list_of_products() {
+
+    int choice;
+    char filename[100];
+
+    printMenu();
+
+    printf("Виберіть пункт меню: ");
+    scanf("%d", &choice);
+
+    if (choice >= 1 && choice <= 7) {
+        const char *filenames[7] = {
+                "Fruits_vegetables.txt",
+                "Meat_fish.txt",
+                "water.txt",
+                "Juice_soft_drinks.txt",
+                "Dairy_products_eggs.txt",
+                "bread_products.txt",
+                "Groceries_cereals_pasta.txt"
+        };
+
+        const char *menuOptions[7] = {
+                "Фрукти та овочі",
+                "М'ясо та риба",
+                "Вода",
+                "Сік та безалкогольні напої",
+                "Молочні продукти та яйця",
+                "Хлібні вироби",
+                "Бакалії, крупи, та макаронні вироби"
+        };
+
+        printf("Ви обрали '%s'.\n", menuOptions[choice - 1]);
+        strcpy(filename, filenames[choice - 1]);
+        readAndProcessFile(filename);
+    } else if (choice == 0) {
+        printf("Ви обрали вихід. До побачення!\n");
+    } else {
+        printf("Невірний вибір. Спробуйте ще раз.\n");
     }
 
     return 0;
