d86231ff6aabc30e2fd0ee9cab5603365ade8693
Oleksandr <sashatroan@gmail.com>
2023-07-14 16:05:44 +0300

    Try to add ukraine laguage

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <stdbool.h>
 #include <conio.h>
 #include <string.h>
+#include <windows.h>
 
 int buying(bool* isEachUserLoggedIn);
 int account_info(bool* isEachUserLoggedIn);
@@ -11,10 +12,12 @@ void recomendation_algoritm();
 
 int main() {
     int menu_item;
+    SetConsoleCP(65001);
+    SetConsoleOutputCP(65001);
     bool isEachUserLoggedIn = false;
     printf("Вітаємо вас у нашому застосунку сервісу замовлення їжі!\n");
     while (true) {
-        printf("Оберіть пункт меню для того щоб продовжити:\n 1. Дії щодо аккаунта\n 2. Інформація щодо товарів\n 3. Купівля\n 4. Вихід\n");
+        printf("Оберіть пункт меню для того щоб продовжити:\n 1. Дії щодо аккаунта\n 2. Інформація щодо товарів\n 3. Купівля\n 5. Вихід\n");
         printf("Введіть цифру пункту меню:\n");
         menu_item = getch();
         switch (menu_item) {
