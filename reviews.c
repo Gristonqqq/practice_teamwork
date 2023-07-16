25910ea9845ff97b481998ae1bba4d5b54dbd778
Вероніка Левіна <ipz22-v.levina@nubip.edu.ua>
2023-07-16 20:36:27 +0300

    Updated the function of adding reviews

--- reviews.c
+++ reviews.c
@@ -5,33 +5,53 @@
 #include <string.h>
 #include <windows.h>
 
-void create_review (char* email_from_login);
+void create_review(char* email_from_login);
+void display_review(char* email_from_login);
 
-int reviews_menu (char* email_from_login, bool* isUserLoggedIn) {
+int reviews_menu(char* email_from_login, bool* isUserLoggedIn) {
     int menu_reviews = 0;
     if (*isUserLoggedIn == true) {
-        while (true){
+        while (true) {
             printf("\n 1. Ввести відгук про послугу\n 2. Показати відгуки\n 3. Вихід\n");
             menu_reviews = getch();
-            switch(menu_reviews){
+            switch (menu_reviews) {
                 case '1':
-                    create_review (email_from_login);
+                    create_review(email_from_login);
                     break;
                 case '2':
+                    display_review(email_from_login);
+                    break;
                 case '3':
                     return 0;
-                default: printf ("Введіть коректну цифру");
+                default:
+                    printf("Введіть коректну цифру");
             }
         }
+    } else {
+        printf("Спочатку увійдіть в акаунт");
     }
-    else printf ("Спочатку увійдіть в акаунт");
 }
-void create_review (char* email_from_login) {
-    char user_review [1000];
-    FILE* review = fopen ("reviews.txt" , "at");
-    fprintf(review , "\nUser: %s\n", email_from_login);
+
+void create_review(char* email_from_login) {
+    char user_review[1000];
+    FILE* review = fopen("reviews.txt", "at");
+    fprintf(review, "\nUser: %s\n", email_from_login);
     printf("Введіть свій коментар:\n");
-    scanf("%s" , user_review);
-    fprintf(review, "Review: %s\n" , user_review);
+    scanf("%s", user_review);
+    fprintf(review, "Review: %s\n", user_review);
+    fclose(review);
+}
+
+void display_review(char* email_from_login) {
+    FILE* review = fopen("reviews.txt", "rt");
+    char line[1000];
+    printf("Відгуки:\n");
+    while (fgets(line, sizeof(line), review) != NULL) {
+        if (strstr(line, email_from_login) != NULL) {
+            printf("%s", line);
+            fgets(line, sizeof(line), review);
+            printf("%s\n", line);
+        }
+    }
     fclose(review);
 }
